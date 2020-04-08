ca82816a0029a0837360795d17ebe887ea7f6506
Ivanna Sivak <isivak@intel471.com>
2020-04-08 22:22:47 +0300

    Add assertion for "Add to basket" button test

--- test_items.py
+++ test_items.py
@@ -1,6 +1,11 @@
 link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
 
 
-def test_add_button(browser):
-    browser.get(link)
-    browser.find_element_by_css_selector(".btn-add-to-basket")
+def test_add_to_basket_button(browser):
+    try:
+        browser.get(link)
+        browser.find_element_by_css_selector(".btn-add-to-basket")
+        result = True
+    except:
+        result = False
+    assert result is True, "Button not found"
